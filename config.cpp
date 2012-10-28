3a9fb8ba844c8084d5bfb4675c62a314f3883328
Dinesh Prasanth <mail2dineshnow@gmail.com>
2012-10-28 20:38:09 +0530

    Automatic generation of random service library using the raw word sets

---
+++ config.cpp
@@ -0,0 +1,5 @@
+using namespace std;
+
+int no_services = 20;	// total number of services
+int ann_per_inf = 4;	// number of words to be annotated per input or output interface
+
---
+++ generateServiceLibrary.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<fstream>
+#include<cstdlib>
+#include<ctime>
+#include "config.cpp"
+using namespace std;
+
+class operation
+{
+	vector<string> annotations;
+};
+
+class service
+{
+	int id;
+	operation input;
+	operation output;
+};
+
+int main()
+{
+	string line;
+	vector <string> source;
+	ifstream srcfile;
+	ofstream destfile;
+	srcfile.open("source.txt");
+	if(srcfile.is_open())
+	{
+		while(srcfile.good())
+		{
+			getline(srcfile,line);
+			source.push_back(line);
+		}
+	}
+	srcfile.close();
+	destfile.open("serviceLibrary.txt",ios::out);
+	int word_pos;	// position of the word from source
+	int source_size = source.size() - 1;
+	operation op;
+	string annotations;
+	srand((unsigned)time(0));
+	if(destfile.is_open())
+	{
+		for(int i=0;i<no_services;i++)
+		{
+			for(int j=0;j<ann_per_inf;j++)
+			{
+				word_pos = rand() % source_size;
+				annotations.append(source[word_pos]+" ");
+			}
+			annotations.append(":");
+			for(int j=0;j<ann_per_inf;j++)
+			{
+				word_pos = rand() % source_size;
+				annotations.append(source[word_pos]+" ");
+			}
+			annotations.append("\n");
+			destfile<<annotations;
+			annotations="";
+		
+		}
+	}
+	destfile.close();
+	return 0;
+}

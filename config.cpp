a1935562a31ae74e303d59cb75761e341f862d78
Dinesh Prasanth <mail2dineshnow@gmail.com>
2012-10-30 18:20:24 +0530

    implemented paolucci algorithm for web service discovery. tested for primary cases

--- config.cpp
+++ config.cpp
@@ -1,11 +1,16 @@
 using namespace std;
 
-string query_in[] = {"price","colour"};
-string query_out[] = {"car","bus","truck"};
+string query_in[] = {"employee","staff"};
+int query_in_size = 2;
+string query_out[] = {"salary","bonus","share"};
+int query_out_size = 3;
 
-/* to check similarity in WordNet : similarity.pl --type WordNet::Similarity::lin tiger#n cat#n */
+/* to check similarity in WordNet : 
+similarity.pl --type WordNet::Similarity::lin tiger#n cat#n
+ */
 
-int op_per_service = 3; // number of operations per service
-int no_ops = 21;	// total number of operations (multiple of op_per_service)
-int ann_per_inf = 4;	// number of words to be annotated per input or output interface
-
+int op_per_service = 1; // number of operations per service
+int no_ops = 25;		// total number of operations (multiple of op_per_service)
+int ann_per_inf = 3;	// number of words to be annotated per input or output interface
+int no_results = 5; 	// Number of matching webservices to be resulted,must be less than no of operations
+double threshold = 0.10;	// minimum threshold in match with the webservices
--- matchmake.cpp
+++ matchmake.cpp
@@ -1,13 +1,17 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<unistd.h>
+#include<stdlib.h>
 #include"config.cpp"
 #include"fstream"
+#include"algorithm"
 using namespace std;
 
 class operation
 {
 	public:
+	int id;
 	int service_id;
 	vector<string> input;
 	vector<string> output;
@@ -18,14 +22,35 @@ class operation
 		output.clear();
 	}
 };
+class matchedOp
+{
+	public:
+	int id;
+	double sum;
+};
 
+vector<matchedOp> SortMatches(vector<matchedOp> matched_ops)
+{
+	int match_size = matched_ops.size();
+	for(int i=0;i<match_size;i++)
+	{
+		for(int j=0;j<match_size;j++)
+		{
+			if(matched_ops[i].sum > matched_ops[j].sum)
+			{
+				swap(matched_ops[i],matched_ops[j]);
+			}
+		}
+	}
+	return matched_ops;
+}
 
 int main()
 {
 	int opt,pos;
 	string line,in_line,out_line;
 	string serviceData[2];
-	cout<<"Enter Algorithm:\n1.Paolucci Algorithm\n2.BruteForce Algorithm\n3.Assignment Algorithm\n: ";
+	cout<<"Enter Algorithm:\n1.Paolucci Algorithm\n2.BruteForce Algorithm\n3.Assignment Algorithm\nChoice : ";
 	l:
 	cin>>opt;
 	vector<operation> serviceLibrary;
@@ -37,12 +62,13 @@ int main()
 		int no_ops_itr = no_ops;
 		int op_per_service_itr = 0;
 		int service_id = 0;
+		int op_id = 0;
 		while(no_ops_itr)
 		{
 			getline(srcfile,line);
 			pos=line.find(":")-1;
 			in_line = line.substr(0,pos);
-			out_line = line.substr(pos+1);
+			out_line = line.substr(pos+2);
 			for(int i=0;i<ann_per_inf;i++)
 			{
 				tempOp.input.push_back(in_line.substr(0,in_line.find(" ")));
@@ -58,16 +84,144 @@ int main()
 				op_per_service_itr = 0;
 				service_id++;
 			}
+			tempOp.id = op_id;
 			tempOp.service_id = service_id;
+			op_id++;
 			op_per_service_itr++;
 			serviceLibrary.push_back(tempOp);
 			tempOp.empty();
 			no_ops_itr--;
 		}
 	}
-	
+	//system("similarity.pl --type WordNet::Similarity::lin tiger#n cat#n > tempSim.txt");
 	if(opt==1)
 	{
+		string sim_str,sim_line;
+		vector <matchedOp> matched_ops;
+		matchedOp tempMatch;
+		double sim_val,sum_in,sum_out;
+		vector<double> sim_vals;
+		int op_size = serviceLibrary.size();
+		for(int i=0;i<op_size;i++)
+		{
+			int j=0;
+			sum_in=0;
+			while(j<query_in_size)
+			{
+				for(int k=0;k<ann_per_inf;k++)
+				{
+					string sim_str = "similarity.pl --type WordNet::Similarity::lin "+query_in[j]+"#n "+serviceLibrary[i].input[k]+"#n > tempSim.txt";
+					char *sim_str_c = new char[sim_str.size()+1];
+					int l;
+					for(l=0;l<sim_str.size();l++)
+					{
+						sim_str_c[l]=sim_str[l];
+					}
+					sim_str_c[l]='\0';
+					system(sim_str_c);
+					cout<<"\n<"<<query_in[j]<<" "<<serviceLibrary[i].input[k]<<">\n";
+					ifstream simFile;
+					simFile.open("tempSim.txt");
+					if(simFile.is_open())
+					{
+						getline(simFile,sim_line);
+						if(sim_line.find("  ")<sim_line.size())
+						{
+							sim_line = sim_line.substr(sim_line.find("  ")+2);
+							sim_line = sim_line.substr(sim_line.find("  ")+2);
+							char *sim_line_c = new char[sim_line.size()];
+							for(l=0;l<sim_line.size();l++)
+							{
+								sim_line_c[l]=sim_line[l];
+							}
+							sim_line_c[l]='\0';
+							sim_val = (double)atof(sim_line_c);
+						}
+						else
+							sim_val=0;
+						sim_vals.push_back(sim_val);
+					}
+					simFile.close();
+				}
+				sum_in += *max_element(sim_vals.begin(),sim_vals.end());
+				
+				sim_vals.clear();
+				j++;
+			}
+			j = 0;
+			sum_out = 0;
+			sim_vals.clear();
+			while(j<query_out_size)
+			{
+				for(int k=0;k<ann_per_inf;k++)
+				{
+					string sim_str = "similarity.pl --type WordNet::Similarity::lin "+query_out[j]+"#n "+serviceLibrary[i].output[k]+"#n > tempSim.txt";
+					char *sim_str_c = new char[sim_str.size()+1];
+					int l;
+					for(l=0;l<sim_str.size();l++)
+					{
+						sim_str_c[l]=sim_str[l];
+					}
+					sim_str_c[l]='\0';
+					system(sim_str_c);
+					cout<<"\n<"<<query_out[j]<<" "<<serviceLibrary[i].output[k]<<">\n";
+					ifstream simFile;
+					simFile.open("tempSim.txt");
+					if(simFile.is_open())
+					{
+						getline(simFile,sim_line);
+						if(sim_line.find("  ")<sim_line.size())
+						{
+							sim_line = sim_line.substr(sim_line.find("  ")+2);
+							sim_line = sim_line.substr(sim_line.find("  ")+2);
+							char *sim_line_c = new char[sim_line.size()];
+							for(l=0;l<sim_line.size();l++)
+							{
+								sim_line_c[l]=sim_line[l];
+							}
+							sim_line_c[l]='\0';
+							sim_val = (double)atof(sim_line_c);
+						}
+						else
+							sim_val=0;
+						sim_vals.push_back(sim_val);
+					}
+					else
+						cout<<"\nUnexpected file open error\n";
+					simFile.close();
+				}
+				sum_out += *max_element(sim_vals.begin(),sim_vals.end());
+				sim_vals.clear();
+				j++;
+			}
+			sum_in = (sum_in*sum_out)/(j*ann_per_inf);	// check for threashold and push to list to sort
+			if(sum_in > threshold)
+			{
+				tempMatch.id = i;
+				tempMatch.sum = sum_in;
+				matched_ops.push_back(tempMatch);
+			}
+			//cout<<"\n<"<<sum_in<<">\n";
+			sum_in=0;
+		}
+		matched_ops = SortMatches(matched_ops);
+		cout<<"\n";
+		int match_size = matched_ops.size();
+		for(int i=0;((i<match_size)&&(i<no_results));i++)
+		{
+			cout<<"Operation ID : "<<matched_ops[i].id<<" Match precision : "<<matched_ops[i].sum<<"\n";
+			cout<<"Input : ";
+			for(int j=0;j<ann_per_inf;j++)
+			{
+				cout<<serviceLibrary[matched_ops[i].id].input[j]<<" ";
+			}
+			cout<<"Output : ";
+			for(int j=0;j<ann_per_inf;j++)
+			{
+				cout<<serviceLibrary[matched_ops[i].id].output[j]<<" ";
+			}
+			cout<<"\n\n";
+		}
 		
 	}
 	else

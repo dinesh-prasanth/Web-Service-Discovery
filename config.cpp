bb06f1c48fda5fa0353e2c38278304bf7c36d913
Dinesh Prasanth <mail2dineshnow@gmail.com>
2012-11-26 09:23:33 +0530

    implemented brute force match making algorithm that checks for al combinotorical possiblities to map with the test annotations

--- config.cpp
+++ config.cpp
@@ -1,8 +1,8 @@
 using namespace std;
 
-string query_in[] = {"employee","staff"};
-int query_in_size = 2;
-string query_out[] = {"salary","bonus","share"};
+string query_in[] = {"employee","staff","manager"};
+int query_in_size = 3;
+string query_out[] = {"salary","bonus","money"};
 int query_out_size = 3;
 
 /* to check similarity in WordNet : 
@@ -10,7 +10,7 @@ similarity.pl --type WordNet::Similarity::lin tiger#n cat#n
  */
 
 int op_per_service = 1; // number of operations per service
-int no_ops = 25;		// total number of operations (multiple of op_per_service)
+int no_ops = 1;		// total number of operations (multiple of op_per_service)
 int ann_per_inf = 3;	// number of words to be annotated per input or output interface
-int no_results = 5; 	// Number of matching webservices to be resulted,must be less than no of operations
-double threshold = 0.10;	// minimum threshold in match with the webservices
+int no_results = 10; 	// Number of matching webservices to be resulted,must be less than no of operations
+double threshold = 0.00;	// minimum threshold in match with the webservices
--- matchmake.cpp
+++ matchmake.cpp
@@ -8,6 +8,9 @@
 #include"algorithm"
 using namespace std;
 
+vector< vector<int> > perm_list;
+vector<int> perm;
+
 class operation
 {
 	public:
@@ -28,6 +31,34 @@ class matchedOp
 	int id;
 	double sum;
 };
+
+void permutations(int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		perm.push_back(i);
+	}
+
+	l:
+	perm_list.push_back(perm);
+	int i=n-2;
+	// FINDING THE LARGEST INDEX i SO THAT P[i] < P[i+1]
+	while((perm[i]>=0)&&(perm[i]>perm[i+1]))
+		--i;
+	if(i<0)
+		return;
+	int k=n-1;
+	// FINDING P[k] after i which is just larger than P[i]
+	while(perm[i]>perm[k])
+		--k;
+	swap(perm[i],perm[k]);
+	i=i+1;
+	k=n-1;
+	int limit = (i+k)/2;
+	for(;i<=limit;i++,k--)
+		swap(perm[i],perm[k]);
+	goto l;
+}
 
 vector<matchedOp> SortMatches(vector<matchedOp> matched_ops)
 {
@@ -53,6 +84,7 @@ int main()
 	cout<<"Enter Algorithm:\n1.Paolucci Algorithm\n2.BruteForce Algorithm\n3.Assignment Algorithm\nChoice : ";
 	l:
 	cin>>opt;
+	/* GENERATION OF SERVICE LIBRARY FROM THE serviceLibrary.txt FILE INTO A VECTOR */
 	vector<operation> serviceLibrary;
 	operation tempOp;
 	ifstream srcfile;
@@ -93,7 +125,7 @@ int main()
 			no_ops_itr--;
 		}
 	}
-	//system("similarity.pl --type WordNet::Similarity::lin tiger#n cat#n > tempSim.txt");
+	// COMMAND LINE CALL similarity.pl --type WordNet::Similarity::lin tiger#n cat#n > tempSim.txt
 	if(opt==1)
 	{
 		string sim_str,sim_line;
@@ -194,14 +226,15 @@ int main()
 				sim_vals.clear();
 				j++;
 			}
-			sum_in = (sum_in*sum_out)/(j*ann_per_inf);	// check for threashold and push to list to sort
-			if(sum_in > threshold)
+			//cout<<"\n"<<sum_in<<" "<<sum_out<<" "<<query_out_size<<ann_per_inf<<" ";
+			sum_in = (sum_in*sum_out)/(query_out_size*ann_per_inf);
+			cout<<sum_in<<" match!\n";
+			if(sum_in > threshold)// check for threshold and push to list to sort
 			{
 				tempMatch.id = i;
 				tempMatch.sum = sum_in;
 				matched_ops.push_back(tempMatch);
 			}
-			//cout<<"\n<"<<sum_in<<">\n";
 			sum_in=0;
 		}
 		matched_ops = SortMatches(matched_ops);
@@ -223,6 +256,202 @@ int main()
 			cout<<"\n\n";
 		}
 		
+	}
+	else if(opt==2)	// BRUTE FORCE ALGORITHM
+	{
+		string sim_str,sim_line;
+		vector <matchedOp> matched_ops;
+		matchedOp tempMatch;
+		int k;
+		vector<double> temp_sim_list;
+		double sim_val,sum_in,sum_out;
+		vector<double> sum_in_list,sum_out_list;
+		vector<double> sim_vals;
+		vector<string> tempList;
+		vector<string> test_operation;
+		int op_size = serviceLibrary.size();
+		permutations(query_in_size);
+		for(int i=0;i<op_size;i++)	// foreach operation
+		{
+			int j=0;
+			sum_in=0;
+			int perm_length = perm_list.size();
+			int perm_itr = 0;
+			while(perm_itr<perm_length)
+			{
+			cout<<perm_itr+1<<" permute\n";
+			tempList.clear();
+			for(int tmp_itr=0;tmp_itr<ann_per_inf;tmp_itr++){
+				tempList.push_back(query_in[perm_list[perm_itr][tmp_itr]]);
+				cout<<query_in[perm_list[perm_itr][tmp_itr]]<<" ";
+				}cout<<"\n";
+			j=0;
+			test_operation = serviceLibrary[i].input;
+			for(int j=0;j<query_in_size;j++)	// foreach target input annotation
+			{
+			k=0;
+			while(k<test_operation.size())	// foreach input annotation in this test operation
+			{
+				
+				string sim_str = "similarity.pl --type WordNet::Similarity::lin "+tempList[j]+"#n "+test_operation[k]+"#n > tempSim.txt";
+				char *sim_str_c = new char[sim_str.size()+1];
+				int l;
+				for(l=0;l<sim_str.size();l++)
+				{
+					sim_str_c[l]=sim_str[l];
+				}
+				sim_str_c[l]='\0';
+				system(sim_str_c);
+				cout<<"<"<<tempList[j]<<" "<<test_operation[k]<<">\n";
+				ifstream simFile;
+				simFile.open("tempSim.txt");
+				if(simFile.is_open())
+				{
+					getline(simFile,sim_line);
+					if(sim_line.find("  ")<sim_line.size())
+					{
+						sim_line = sim_line.substr(sim_line.find("  ")+2);
+						sim_line = sim_line.substr(sim_line.find("  ")+2);
+						char *sim_line_c = new char[sim_line.size()];
+						for(l=0;l<sim_line.size();l++)
+						{
+							sim_line_c[l]=sim_line[l];
+						}
+						sim_line_c[l]='\0';
+						sim_val = (double)atof(sim_line_c);
+					}
+					else
+						sim_val=0;
+				}
+				simFile.close();
+				temp_sim_list.push_back(sim_val);	
+				cout<<sim_val<<"\n";
+				k++;
+			}
+			double sim_max = temp_sim_list[0];
+			int sim_max_index=0;
+			for(int m=1;m<temp_sim_list.size();m++)
+			{
+				if(temp_sim_list[m]>sim_max)
+				{sim_max_index=m;sim_max=temp_sim_list[m];}
+			}
+			test_operation.erase(test_operation.begin()+sim_max_index);
+			temp_sim_list.clear();
+			sum_in += sim_max;
+			cout<<sim_max<<" max\n";
+			}
+				
+			sum_in_list.push_back(sum_in);
+			sum_in = 0;
+			perm_itr++;
+			}
+			
+			j = 0;
+			sum_out = 0;
+			perm_itr=0;
+			while(perm_itr<perm_length)
+			{
+			cout<<perm_itr+1<<" permute\n";
+			tempList.clear();
+			for(int tmp_itr=0;tmp_itr<ann_per_inf;tmp_itr++){
+				tempList.push_back(query_out[perm_list[perm_itr][tmp_itr]]);
+				cout<<query_out[perm_list[perm_itr][tmp_itr]]<<" ";
+				}cout<<"\n";
+			test_operation = serviceLibrary[i].output;
+			for(int j=0;j<query_out_size;j++)	// foreach target input annotation
+			{
+			k=0;
+			while(k<test_operation.size())	// foreach input annotation in this test operation
+			{
+				string sim_str = "similarity.pl --type WordNet::Similarity::lin "+tempList[j]+"#n "+test_operation[k]+"#n > tempSim.txt";
+				char *sim_str_c = new char[sim_str.size()+1];
+				int l;
+				for(l=0;l<sim_str.size();l++)
+				{
+					sim_str_c[l]=sim_str[l];
+				}
+				sim_str_c[l]='\0';
+				system(sim_str_c);
+				cout<<"\n<"<<tempList[j]<<" "<<test_operation[k]<<">\n";
+				ifstream simFile;
+				simFile.open("tempSim.txt");
+				if(simFile.is_open())
+				{
+					getline(simFile,sim_line);
+					if(sim_line.find("  ")<sim_line.size())
+					{
+						sim_line = sim_line.substr(sim_line.find("  ")+2);
+						sim_line = sim_line.substr(sim_line.find("  ")+2);
+						char *sim_line_c = new char[sim_line.size()];
+						for(l=0;l<sim_line.size();l++)
+						{
+							sim_line_c[l]=sim_line[l];
+						}
+						sim_line_c[l]='\0';
+						sim_val = (double)atof(sim_line_c);
+					}
+					else
+						sim_val=0;
+				}
+				else
+					cout<<"\nUnexpected file open error\n";
+				simFile.close();
+				temp_sim_list.push_back(sim_val);	
+				cout<<sim_val<<"\n";
+				k++;
+			}
+			//sim_val = *max_element(temp_sim_list.begin(),temp_sim_list.end());
+			double sim_max = temp_sim_list[0];
+			int sim_max_index=0;
+			for(int m=1;m<temp_sim_list.size();m++)
+			{
+				if(temp_sim_list[m]>sim_max)
+				{sim_max_index=m;sim_max=temp_sim_list[m];}
+			}
+			test_operation.erase(test_operation.begin()+sim_max_index);
+			temp_sim_list.clear();
+			sum_out += sim_max;
+			cout<<sim_max<<" max\n";
+			}
+			sum_out_list.push_back(sum_out);
+			sum_out=0;
+			perm_itr++;
+			}
+			
+			sum_in = *max_element(sum_in_list.begin(),sum_in_list.end());
+			sum_out = *max_element(sum_out_list.begin(),sum_out_list.end());
+			cout<<"\n"<<sum_in<<" "<<sum_out<<" "<<query_out_size<<" "<<ann_per_inf<<" ";
+			sum_in = (sum_in*sum_out)/(query_out_size*ann_per_inf);
+			cout<<sum_in<<" match!\n";
+			if(sum_in > threshold)// check for threshold and push to list to sort
+			{
+				tempMatch.id = i;
+				tempMatch.sum = sum_in;
+				matched_ops.push_back(tempMatch);
+			}
+			sum_in_list.clear();
+			sum_out_list.clear();
+			sum_in = 0;sum_out = 0;
+		}
+		perm_list.clear();
+		matched_ops = SortMatches(matched_ops);
+		cout<<"\n";
+		int match_size = matched_ops.size();
+		for(int i=0;((i<match_size)&&(i<no_results));i++)
+		{
+			cout<<"Operation ID : "<<matched_ops[i].id<<" Match precision : "<<matched_ops[i].sum<<"\n";
+			cout<<"Input : ";
+			for(int j=0;j<ann_per_inf;j++)
+			{
+				cout<<serviceLibrary[matched_ops[i].id].input[j]<<" ";
+			}
+			cout<<"Output : ";
+			for(int j=0;j<ann_per_inf;j++)
+			{
+				cout<<serviceLibrary[matched_ops[i].id].output[j]<<" ";
+			}
+			cout<<"\n\n";
+		}
 	}
 	else
 	{
